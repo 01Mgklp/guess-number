4aeb8e97a399a78d2d31986647403a6b5da67758
talha <mt.business45@gmail.com>
2023-11-04 12:58:10 +0300

    click enter

--- app.js
+++ app.js
@@ -24,17 +24,59 @@ document.querySelector('.check').addEventListener("click", () => {
         document.querySelector('.number').textContent = rastgeleSayi
 
         // top score kontrolu
+
+        if (skor > enYuksekSkor){
+            enYuksekSkor = skor
+            document.querySelector('.top-score').textContent = enYuksekSkor
+        }
+
     } else {
 
         //? skor 1 den buyuk oldugu surece tahmin hakkim var
 
-        if (skor > 1){
-            skor --
+        if (skor > 1) {
+            skor--
             document.querySelector('.score').textContent = skor
+
+            tahmin < rastgeleSayi
+                ? mesaj.textContent = 'Arttir 👆'
+                : mesaj.textContent = 'Azalt 👇'
         } else {
             mesaj.textContent = 'Oyunu Kaybettiniz 😥'
             document.querySelector('.score').textContent = 0
             document.querySelector('body').style.backgroundColor = 'red'
         }
+    }
+})
+
+//? Again butonuna basinca ayarlar baslangic degerine gelsin. arka plan #2d3436 olsun
+
+document.querySelector('.again').onclick = () => {
+
+    document.querySelector('body').style.backgroundColor = '#2d3436'
+
+    rastgeleSayi = Math.ceil(Math.random() * 20)
+    console.log(rastgeleSayi);
+
+    skor = 10
+
+    document.querySelector('.score').textContent = skor
+
+    document.querySelector('.number').textContent = '?'
+
+    document.querySelector('.guess').value = ''
+
+    mesaj.textContent = 'Oyun Yeni Oyuncu Icin Basliyor'
+
+}
+
+//! ENTER
+
+// klavyeden enter butonuna basildiginda check butonu calissin
+
+document.addEventListener('keydown', function(e){
+    if (e.key === 'Enter') {
+        // enter tusuna basildiginda check butonuna tikla
+        document.querySelector('.check').click()
     }
 })
